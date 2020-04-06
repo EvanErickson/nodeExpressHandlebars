4d7ee33c91bb51ca91a99489a9fe99f60122dff6
EvanErickson <evanserickson@gmail.com>
2020-04-06 11:56:54 -0700

    added

---
+++ config/connection.js
@@ -0,0 +1,19 @@
+
+const { createConnection } = require('mysql2');
+
+let connection;
+
+if (process.env.JAWSDB_URL){
+  connection = createConnection(process.env.JAWSDB_URL);
+}
+else{
+ connection = createConnection({
+  host: process.env.DB_HOST,
+  port: 3306,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: 'burgers_db'
+})
+}
+
+module.exports = connection;
